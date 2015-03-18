78bdfbe52c6731089ffe9336e8d797afb331d193
buildsample <buildsampletest@gmail.com>
2015-03-18 15:07:29 +0530

    Update couch.py

--- couch.py
+++ couch.py
@@ -14,7 +14,8 @@ class Couch():
             {"name": "Lakshmi"},
             {"name": "Ganesha"},
             {"name": "Krishna"},
-            {"name": "Murugan"}
+            {"name": "Murugan"},
+            {"name": "sample"}
         ]
         self.db.save_docs(things)
 
